f9c724c77dcd5f40a7e79de789ecc8f2aaffb75e
Arindam Pal <arindampal2000@gmail.com>
2024-10-08 18:25:59 +0530

    01 sayHello

---
+++ src/sayHello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    std::string input;
+    std::cin >> input;
+    std::cout << "Hello, " << input << "\n";
+
+    return 0;
+}
